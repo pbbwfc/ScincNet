ff627803f927e78ed7395151195bfb66db862f64
Phil Brooks <phil.brooks@asr-services.com>
2020-12-17 15:14:24 +0000

    Add all forms

---
+++ srcNet/FsChess.WinForms/PnlAnl.fs
@@ -0,0 +1,85 @@
+﻿namespace FsChess.WinForms
+
+open System.Drawing
+open System.Windows.Forms
+//open State
+
+[<AutoOpen>]
+module Library7 =
+    type PnlAnl() as this =
+        inherit Panel(Width = 400)
+        let pnlt = new Panel(Dock = DockStyle.Top, Height = 30)
+        let albl = 
+            new Label(Dock = DockStyle.Top, Text = "Engine Stopped", 
+                      TextAlign = ContentAlignment.BottomLeft)
+        let sbtn = 
+            new System.Windows.Forms.Button(Text = "Start", 
+                                            Dock = DockStyle.Right)
+        let dg = 
+            new WebBrowser(AllowWebBrowserDrop = false,IsWebBrowserContextMenuEnabled = false,WebBrowserShortcutsEnabled = false,Dock=DockStyle.Fill)
+
+        let nl = System.Environment.NewLine
+        let hdr = 
+            "<html><body>" + nl +
+            "<table style=\"width:100%;border-collapse: collapse;\">" + nl +
+            "<tr><th style=\"text-align: left;\">Score</th><th style=\"text-align: left;\">Line</th>" + nl
+
+        let ftr = 
+            "</table>" + nl +
+            "</body></html>" + nl
+  
+        //set up analysis
+        let setAnl start = 
+            
+            let setanl() = 
+                if start then 
+                    dg.DocumentText <- hdr + ftr
+            if (this.InvokeRequired) then 
+                try 
+                    this.Invoke(MethodInvoker(fun () -> setanl())) |> ignore
+                with _ -> ()
+            else setanl()
+    
+        //set header
+        let setHdr msg = 
+            if (this.InvokeRequired) then 
+                try 
+                    this.Invoke(MethodInvoker(fun () -> albl.Text <- msg)) 
+                    |> ignore
+                with _ -> ()
+            else albl.Text <- msg
+    
+        //add Message
+        let addMsg (scr,ln) = 
+            let addmsg() = 
+                let nr = "<tr><td style='width: 50px;'>" + scr + "</td><td>" + ln + "</td></tr>" + nl
+                
+                dg.DocumentText <- hdr + nr + ftr
+                
+            if (this.InvokeRequired) then 
+                try 
+                    this.Invoke(MethodInvoker(fun () -> addmsg())) |> ignore
+                with _ -> ()
+            else addmsg()
+
+        //start or stop
+        let startstop() =
+            if sbtn.Text="Start" then
+                sbtn.Text<-"Stop"
+                //sst.AnlpStart()
+            else
+                sbtn.Text<-"Start"
+                //sst.AnlpStop()
+    
+        do 
+            this.Controls.Add(dg)
+            pnlt.Controls.Add(albl)
+            pnlt.Controls.Add(sbtn)
+            this.Controls.Add(pnlt)
+            setAnl(true)
+            //events
+            //sst.AnlpChng |> Observable.add setAnl
+            //sst.AnlpHeadChng |> Observable.add setHdr
+            //sst.AnlpMsg |> Observable.add addMsg
+            sbtn.Click.Add(fun _ -> startstop())
+ 
--- srcNet/ScincNet/Form.fs
+++ srcNet/ScincNet/Form.fs
@@ -14,31 +14,53 @@ module Form =
         let mutable gms = []
         let mutable ct = 0
      
-        let bd,pgn = CreateLnkBrdPgn()
-        let pnl = new Panel(Dock=DockStyle.Top,Height=30)
-        let nmlbl =
-             new Label(Text = "Not loaded", Dock = DockStyle.Left, 
-                       TextAlign = ContentAlignment.MiddleLeft,Width=400)
-        let ts =
-            new ToolStrip(Anchor = AnchorStyles.Right, 
-                          GripStyle = ToolStripGripStyle.Hidden, 
-                          Dock = DockStyle.None, Left = 100)
+        let bd = new PnlBoard(Dock=DockStyle.Fill)
+        let pgn = new WbPgn(Dock=DockStyle.Fill)
+        let sts = new WbStats(Dock=DockStyle.Fill)
+        let gms = new DgvGames(Dock=DockStyle.Fill)
+        let anl = new PnlAnl(Dock=DockStyle.Fill)
 
-        let rtpnl = new Panel(Dock=DockStyle.Fill)
+
+
         //TODO
         let tb = new ToolStrip()
         let mm = new MenuStrip()
 
 
 
+        let gmlbl = new Label(Text="Game: White v. Black",Width=400,TextAlign=ContentAlignment.MiddleLeft,Font = new Font(new FontFamily("Arial"), 12.0f))
+        //TODO need to set this
+        let fllbl = new Label(Text="Filter: 99999999/99999999",Width=400,TextAlign=ContentAlignment.MiddleLeft,Font = new Font(new FontFamily("Arial"), 12.0f))
+        let vpnl = new FlowLayoutPanel(Dock=DockStyle.Fill,FlowDirection=FlowDirection.TopDown)
+
+        let bgpnl = new Panel(Dock=DockStyle.Fill,BorderStyle=BorderStyle.Fixed3D)
+        let lfpnl = new Panel(Dock=DockStyle.Left,BorderStyle=BorderStyle.Fixed3D,Width=400)
+        let rtpnl = new Panel(Dock=DockStyle.Fill,BorderStyle=BorderStyle.Fixed3D)
+        let lftpnl = new Panel(Dock=DockStyle.Top,BorderStyle=BorderStyle.Fixed3D,Height=400)
+        let lfmpnl = new Panel(Dock=DockStyle.Top,BorderStyle=BorderStyle.Fixed3D,Height=50)
+        let lfbpnl = new Panel(Dock=DockStyle.Fill,BorderStyle=BorderStyle.Fixed3D)
+        let rttpnl = new Panel(Dock=DockStyle.Top,BorderStyle=BorderStyle.Fixed3D,Height=350)
+        let rtmpnl = new Panel(Dock=DockStyle.Top,BorderStyle=BorderStyle.Fixed3D,Height=100)
+        let rtbpnl = new Panel(Dock=DockStyle.Fill,BorderStyle=BorderStyle.Fixed3D)
+ 
         do
-            pgn|>rtpnl.Controls.Add
-            //[ homeb; prevb; nextb; endb ] 
-            //|> List.iter (fun c -> ts.Items.Add(c) |> ignore)
-            pnl.Controls.Add(ts)
-            pnl.Controls.Add(nmlbl)
-            pnl|>rtpnl.Controls.Add
-            rtpnl|>this.Controls.Add
-            bd|>this.Controls.Add
+            gms|>rtbpnl.Controls.Add
+            rtbpnl|>rtpnl.Controls.Add
+            anl|>rtmpnl.Controls.Add
+            rtmpnl|>rtpnl.Controls.Add
+            pgn|>rttpnl.Controls.Add
+            rttpnl|>rtpnl.Controls.Add
+            rtpnl|>bgpnl.Controls.Add
+            sts|>lfbpnl.Controls.Add
+            lfbpnl|>lfpnl.Controls.Add
+            gmlbl|>vpnl.Controls.Add
+            fllbl|>vpnl.Controls.Add
+            vpnl|>lfmpnl.Controls.Add
+            lfmpnl|>lfpnl.Controls.Add
+            bd|>lftpnl.Controls.Add
+            lftpnl|>lfpnl.Controls.Add
+            lfpnl|>bgpnl.Controls.Add
+            bgpnl|>this.Controls.Add
             tb|>this.Controls.Add
             mm|>this.Controls.Add
+    
